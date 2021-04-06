30f78bdab51fa21ffac61ac3d1161aac39aa50d4
RezaRfahi <refahireza82@gmail.com>
2021-04-06 23:34:40 +0430

    Commited webdata.py until list build def without sort

--- webdata.py
+++ webdata.py
@@ -1,10 +1,18 @@
 import requests
 import bs4
 import  re
+def build(lis):
+    count=1
+    attrs=list()
+    for i in lis:
+        if count%3!=0:
+            attrs.append(i)
+        count=count+1
+    return attrs
 req=requests.get('https://www.expat.com/en/housing/middle-east/iran/tehran/')
 r=lambda x:'Get Url' if x==200  else 'Get Url Failed!'
 print(r(req.status_code))
 sort=bs4.BeautifulSoup(req.text,'html.parser')
 find=sort.find_all('span', attrs={'class':'classified-wrapper__category'})
 find_attrs=re.findall(r'[0-9]+',str(find))
-print(find_attrs)
+print(build(find_attrs))
