b36de6c9cd3cf7a8253b66965bc520eaa5e1fb52
RezaRfahi <refahireza82@gmail.com>
2021-04-09 23:48:37 +0430

    rename orginal.py to index.py and get eur price in webdata

--- webdata.py
+++ webdata.py
@@ -16,19 +16,30 @@ def build(lis):
                 varlist=[]
         cunt=cunt+1
     return attrs
+####################################################
 def sort_price_array(list_price):
     price=list()
     for i in list_price:
         price.append(int(i.replace(',', '')))
     return price
+####################################################
 def change_to_rial(list_price,list_unit):
     req_dollar=requests.get('https://www.tgju.org/')
     sort_dollar=bs4.BeautifulSoup(req_dollar.text,'html.parser')
     find_dollar_text=sort_dollar.find_all('li',attrs={'id':'l-price_dollar_rl'})
     find_dollar_text_bs=bs4.BeautifulSoup(str(find_dollar_text),'html.parser')
     find_dollar_price=find_dollar_text_bs.find_all('span',attrs={'class':'info-price'})
-    dollar_price=(re.findall(r'.(\d+.+\d).+',str(find_dollar_price)))
+    dollar_price=re.findall(r'.(\d+.+\d).+',str(find_dollar_price))
     dollar_price=int(dollar_price[0].replace(',', ''))
+####
+    req_eur=requests.get('https://www.tgju.org/currency')
+    sort_eur=bs4.BeautifulSoup(req_eur.text,'html.parser')
+    find_eur_text=sort_eur.find_all('tr',attrs={'data-market-row':'price_eur'})
+    find_eur_text_bs=bs4.BeautifulSoup(str(find_eur_text),'html.parser')
+    find_eur_price=find_eur_text_bs.find_all('td',attrs={'class':'nf'})
+    eur_price=re.findall(r'.+.>(\d+.+\d+.)<.+.',str(find_eur_price))
+    eur_price=int(eur_price[0].replace(',',''))
+####################################################
 req=requests.get('https://www.expat.com/en/housing/middle-east/iran/tehran/')
 r=lambda x:'Get Url' if x==200  else 'Get Url Failed!'
 print(r(req.status_code))
