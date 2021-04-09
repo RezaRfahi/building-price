2b54948a226483d083671fb74c194ab76aed9c79
RezaRfahi <refahireza82@gmail.com>
2021-04-09 20:25:22 +0430

    get price , unit , duration in webdata.py

--- webdata.py
+++ webdata.py
@@ -16,10 +16,20 @@ def build(lis):
                 varlist=[]
         cunt=cunt+1
     return attrs
+def sort_price_array(lis):
+    a=1
 req=requests.get('https://www.expat.com/en/housing/middle-east/iran/tehran/')
 r=lambda x:'Get Url' if x==200  else 'Get Url Failed!'
 print(r(req.status_code))
 sort=bs4.BeautifulSoup(req.text,'html.parser')
 find=sort.find_all('span', attrs={'class':'classified-wrapper__category'})
 find_attrs=re.findall(r'[0-9]+',str(find))
-print(build(find_attrs))
+values=build(find_attrs)
+print(values)
+find_price_text=sort.find_all('span', attrs={'class':'classified-wrapper__price'})
+find_price=re.findall(r'\s+.*\s(\d.\d+).per.\w+',str(find_price_text))
+find_unit=re.findall(r'\s+(.*)\s\d+.+\d.per.\w+',str(find_price_text))
+find_dur=re.findall(r'\s+.*\s\d.\d+.per.(\w+)',str(find_price_text))
+print(find_price)
+print(find_unit)
+print(find_dur)
